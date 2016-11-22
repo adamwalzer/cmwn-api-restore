7ab2990aad57b799cdb4bccda451246300663647
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-11-22 16:23:31 -0500

    restore module

--- api-restore/Module.php
+++ api-restore/Module.php
@@ -1,3 +1,20 @@
 <?php
 
-require 'src/Restore/Module.php';
+namespace Restore;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Class Module
+ * @package Restore
+ */
+class Module implements ConfigProviderInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . 'config/module.config.php';
+    }
+}
